422c29fc361b2260bf05b690b7b914ddd1f1fe82
Vasileios Naskos <vasileios.naskos@sonarsource.com>
2025-11-11 08:21:23 +0100

    SLVSROS-1 Update code file license headers to "SonarSource Sàrl"

--- sonarqube-ide-visualstudio-roslyn-plugin/src/main/java/org/sonarsource/sonarlint/visualstudio/roslyn/http/AnalyzerInfoDto.java
+++ sonarqube-ide-visualstudio-roslyn-plugin/src/main/java/org/sonarsource/sonarlint/visualstudio/roslyn/http/AnalyzerInfoDto.java
@@ -1,6 +1,6 @@
 /*
  * SonarQube Ide VisualStudio Roslyn Plugin
- * Copyright (C) 2025-2025 SonarSource SA
+ * Copyright (C) 2025-2025 SonarSource Sàrl
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
